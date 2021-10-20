0ece3ae966e8b256a32e360490856aeee89e76ff
DoggySazHi <william.le.huy@gmail.com>
2021-10-19 23:36:13 -0700

    Copying all of the relevant HTML files from Modulr's HTML "engine".

--- ScrapperCore/Program.cs
+++ ScrapperCore/Program.cs
@@ -4,7 +4,7 @@ using ScrapperCore.Utilities;
 
 namespace ScrapperCore
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
--- ScrapperCore/Startup.cs
+++ ScrapperCore/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ScrapperCore.Utilities;
 
 namespace ScrapperCore
 {
@@ -13,6 +14,7 @@ namespace ScrapperCore
         {
             services.AddControllers();
             services.AddHttpClient();
+            services.AddSingleton(ScrapperConfig.Load());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
